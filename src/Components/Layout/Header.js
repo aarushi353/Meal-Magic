aa789a4442edb2086e8fad165e94c726673211f0
aarushi353 <aarushiabrol999@gmail.com>
2021-08-17 03:06:59 +0530

    Cart button added and header made responsive

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 const Header = (props) => {
@@ -6,7 +7,7 @@ const Header = (props) => {
     <>
       <header className={classes.header}>
           <h1>Meal Magic</h1>
-          <button>Cart</button>
+          <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
           <img src={mealsImage} alt="Delicious food!" />
