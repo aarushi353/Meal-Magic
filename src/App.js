30424b43edaed451a0756ff6fafa7cbea6a906b0
aarushi353 <aarushiabrol999@gmail.com>
2021-08-17 12:06:35 +0530

    added some meal related components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./Components/Layout/Header";
-
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
