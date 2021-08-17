a9ba8537139f431c83a328a9d5a23cd97b34ecff
aarushi353 <aarushiabrol999@gmail.com>
2021-08-17 21:36:46 +0530

    Added Cart and modal component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { Fragment } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
-
+import Cart from "./Components/Cart/Cart";
 function App() {
   return (
-    <>
+    <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
       </main>
-    </>
+    </Fragment>
   );
 }
 
