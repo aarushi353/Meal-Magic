8873ca73e3d6517ab729a3c076921c1e1f0e26bb
aarushi353 <aarushiabrol999@gmail.com>
2021-08-19 12:29:50 +0530

    Add and Remove button in cart made functional

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -13,7 +13,7 @@ const cartReducer = (state, action) => {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
     const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id == action.item.id
+      (item) => item.id === action.item.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
@@ -28,10 +28,33 @@ const cartReducer = (state, action) => {
     } else {
       updatedItems = state.items.concat(action.item);
     }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
+  }
+  if(action.type === 'REMOVE'){
+    
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+
+    if(existingItem.amount === 1){
+    updatedItems = state.items.filter(item => item.id!== action.id);
+    }
+    else{
+    const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+    updatedItems = [...state.items];
+    updatedItems[existingCartItemIndex] = updatedItem;
+  }
+  return{
+    items: updatedItems,
+    totalAmount: updatedTotalAmount
+  }
   }
   return defaultCartState;
 };
