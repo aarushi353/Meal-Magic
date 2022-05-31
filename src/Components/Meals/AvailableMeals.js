cb2f4ff36d38d51986e687d4991763cb93692dd0
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-05-31 14:22:55 +0530

    error handling

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -6,10 +6,16 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
+  const[httpError, setHttpError] = useState();
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://meal-magic-6086c-default-rtdb.firebaseio.com/meals.json');
+      
+      if(!response.ok){
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
       const loadedMeals = [];
       for (const key in responseData) {
@@ -22,11 +28,26 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+      fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+  if(isLoading){
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
 
+
+  if(httpError){
+    return <section className={classes.MealsError}>
+      <p>{httpError}</p>
+    </section>
+  }
   const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
